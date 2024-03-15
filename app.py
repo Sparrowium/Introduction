914481a167674f4c632a73dd29d75f31f657153b
Minh Pham <109504358+Sparrowium@users.noreply.github.com>
2024-03-14 23:00:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
 st.set_page_config(page_title="Welcome")
+
+with st.container():
+    st.write("Hi, My name is Minh Pham")
+    st.write("Am a student from US")
+    st.write("I am passionate about findings news stuff to learn and utilize Python to be more professionally")
+
+with st.container():
+    st.write("------------------------------------------------")
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.write("What I do")
+        st.write("##")
+        st.write("On my github pages I am developing softwares and webpages")
